69fc1c584819b6bf4aa7479ded69c9d2599433bb
hashymozy <asherimusa505@gmail.com>
2025-04-15 11:44:08 +0300

    add store

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,68 @@
-import { Stack } from "expo-router";
+import "@/global.css";
+import React, { useCallback, useState, useEffect } from "react";
+import "react-native-get-random-values";
+import "@/reanimatedConfig";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Provider } from "react-redux";
+import "react-native-reanimated";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Slot } from "expo-router";
+import { store } from "@/store/store";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+const RootLayout: React.FC = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [fontsLoaded] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  useEffect(() => {
+    const prepareApp = async () => {
+      try {
+        if (!fontsLoaded) return;
+      } catch (e) {
+        // console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    };
+
+    prepareApp();
+  }, [fontsLoaded]);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) return null;
+
+  return (
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <GestureHandlerRootView style={{ flex: 1 }} onLayout={onLayoutRootView}>
+          <Slot />
+        </GestureHandlerRootView>
+      </SafeAreaProvider>
+    </Provider>
+  );
+};
+
+export default RootLayout;
